3079b29bd1d70a2342c6fb4583e296228e1cd724
Julien TANGUY <tanguyjulien@hotmail.fr>
2024-10-15 16:56:32 +0200

    amélioration barre de menu bouton rond et regex du téléphone formulaire ajout d'une personne

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,54 +1,67 @@
-import { Box, IconButton, useTheme } from '@mui/material';
-import { useContext } from 'react';
-import { ColorModeContext, tokens } from '../../theme';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import InputBase from '@mui/material/InputBase';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import SearchIcon from '@mui/icons-material/Search';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import { Box, IconButton, useTheme } from "@mui/material";
+import InputBase from "@mui/material/InputBase";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
 
 const Topbar = ({ toggleDrawer }) => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
-    return (
-        <Box display="flex" justifyContent="space-between" p={2}>
-            {/* Menu Icon */}
-            <Box display='flex' justifyContent='start'>
-                <IconButton onClick={toggleDrawer}>
-                    <MenuOutlinedIcon fontSize='large' sx={{ mr: '15px' }} />
-                </IconButton>
-                
-                {/* Search Bar */}
-                <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-                    <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Rechercher" />
-                    <IconButton type="button" sx={{ p: 1 }}>
-                    <SearchIcon />
-                    </IconButton>
-                </Box>
-            </Box>
+  return (
+    <Box
+      display="flex"
+      alignItems={"center"}
+      justifyContent="space-between "
+      p={2}
+    >
+      {/* Menu Icon */}
+      <Box display="flex" justifyContent="start" alignItems={"center"} gap={4}>
+        <IconButton onClick={toggleDrawer}>
+          <MenuOutlinedIcon fontSize="large" />
+        </IconButton>
 
-            {/* Icons */}
-            <Box display="flex">
-                <IconButton onClick={colorMode.toggleColorMode}>
-                {theme.palette.mode === 'dark' ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
-                </IconButton>
-                <IconButton>
-                <NotificationsOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                <SettingsOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                <PersonOutlinedIcon />
-                </IconButton>
-            </Box>
+        {/* Search Bar */}
+        <Box
+          display="flex"
+          backgroundColor={colors.primary[400]}
+          borderRadius="3px"
+        >
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Rechercher" />
+          <IconButton type="button">
+            <SearchIcon />
+          </IconButton>
         </Box>
-    );
+      </Box>
+
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Topbar;
